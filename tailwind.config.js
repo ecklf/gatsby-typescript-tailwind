c586e0fe0f2873d0081158ae36f71033a7c76cc7
ecklf <ecklf@icloud.com>
2021-05-26 00:40:48 +0200

    build: update deps, use tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require("tailwindcss/colors")
 
 module.exports = {
   purge: ["public/**/*.html", "src/**/*.tsx"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
